2185d2aa1bd08ddbb7fc4df518615d368d023b3e
unknown <shubhambindal2999@gmail.com>
2026-05-05 11:36:51 +0530

    add Conversions file

---
+++ 01-Assignment-variables/Conversions.py
@@ -0,0 +1,39 @@
+# 17. Convert boolean to integer Input: 
+# True Output: ? 
+
+a = True
+b=int(a)
+print(b)
+
+# 18. Convert float to string 
+# Input: 45.67 
+# Output: ? 
+
+num = 45.67
+result = str(num)
+print(type(result))
+
+# 19. Convert 20°C to Fahrenheit 
+# Input: 20°C 
+# Output: ?
+
+input = 20
+F= 20*(9/5)+32 
+print(F" Fahrenheit - {F}")
+
+# 20. Convert 50°F to Celsius 
+# Input: 50°F 
+# Output: ?
+
+input = 50
+C = (50-32)*5/9
+print(F" Celsius - {C}")
+
+# 21. Convert integer to binary 
+# Input: 25 
+# Output: ?
+
+input = 25
+binary = bin(input)
+print(F" Binary - {binary}")
+ 
