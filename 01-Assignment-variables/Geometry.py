50623f0ac3f8ef7544c915247f7461e3c7dfa07e
unknown <shubhambindal2999@gmail.com>
2026-05-05 12:06:33 +0530

    add Geometry file

---
+++ 01-Assignment-variables/Geometry.py
@@ -0,0 +1,49 @@
+# 21. Calculate area of a triangle Input: 
+# base = 10, 
+# height = 6 
+# Output: Area = ? 
+
+base = 10
+height = 6
+
+Area =  0.5 * base * height
+print(F" Area of Triangle - {Area}")
+
+# 22. Calculate perimeter of a square Input: 
+# side = 9 
+# Output: Perimeter = ? 
+
+side = 9
+perimeter = 4 * side
+print(F" Perimeter of Square - {perimeter}")
+
+
+# 23. Calculate diameter of a circle Input: 
+# radius = 14 
+# Output: Diameter = ? 
+
+radius = 14
+diameter = 2* radius
+print(F" Diameter of a Circle - {diameter} ")
+
+# 24. Calculate volume of a cube Input: 
+# side = 5 
+# Output: Volume = ?
+
+side = 5
+volume = side * side * side 
+print(F" Volume of Cube - {volume}") 
+
+# 25. Calculate surface area of a cuboid 
+# Input: l = 4, 
+# b = 3, 
+# h = 2 
+# Output: Surface Area = ? 
+
+l = 4
+b = 3
+h = 2
+surface_area = 2 * (l*b + b*h + l*h)
+print(F" Surface area of Cuboid - {surface_area}")
+
+
