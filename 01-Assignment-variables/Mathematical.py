9c86972bcd197eb88ef4c2b5ba8b20ae186e345f
unknown <shubhambindal2999@gmail.com>
2026-05-05 12:26:16 +0530

    Added file Mathematical

---
+++ 01-Assignment-variables/Mathematical.py
@@ -0,0 +1,49 @@
+# 26.Square of sum: (x + y)² 
+# Input: x = 5, 
+# y = 7 
+# Output: ? 
+
+x = 5
+y = 7
+square = (x + y)**2
+print(F" Square of Sum - {square}")
+
+# 27.Simplify expression: 
+# x² - 4x + 4 Input: 
+# x = 3 
+# Output: ? 
+
+
+x = 3
+simplify = 3*3 - 4*3 + 4
+print(F" Simplify the expression - {simplify}")
+
+# 28.Evaluate: 
+# (a + b)(a - b) Input: a = 6, b = 2 Output: ?
+
+a = 6
+b = 2
+result = (a + b) * (a - b)
+print(F" Evaluation - {result}")
+
+
+# 29.Sum of cubes: a³ + b³ Input: a = 1, b = 2 Output: ? 
+
+a = 1
+b = 2
+sum = a*a*a + 2*2*2
+print(F" Sum of Cubes - {sum}")
+
+# 30.Simplify: (x - y)² Input: x = 10, y = 6 Output: ? 
+
+x = 10
+y = 6
+res = (x - y)**2
+print(F"Simplyfies - {res}")
+
+# 31.Difference of cubes: x³ - y³ Input: x = 4, y = 1 Output: ? 
+
+x = 4
+y = 1
+diff = (x**3 -y**3)
+print(F" Difference of cubes - {diff} ")
