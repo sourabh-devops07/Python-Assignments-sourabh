7b915f98803ddc5fb3aa7c8b5f15f305ade37b29
unknown <shubhambindal2999@gmail.com>
2026-05-05 09:30:49 +0530

    add some program

--- 01-Assignment-variables/Finance & Business Calculations.py
+++ 01-Assignment-variables/Finance & Business Calculations.py
@@ -11,3 +11,15 @@ print(F" Profit - {profit}")
 Loss = cost_price - selling_price
 Loss = Loss / selling_price * 100
 print(F" Loss - {Loss}")
+
+# 13. Calculate simple interest 
+# Input: principal = 10000, 
+# rate = 5, 
+# time = 2 
+# Output: Simple Interest = ? 
+
+principal = 10000
+rate = 5
+time = 2
+SI = principal * rate * time /100
+print(F"Simpe Intrest - {SI}")
--- 01-Assignment-variables/tempCodeRunnerFile.py
+++ 01-Assignment-variables/tempCodeRunnerFile.py
@@ -1,8 +1,5 @@
-cost_price = 500
-selling_price = 600 
-profit = selling_price - cost_price
-profit = profit / cost_price * 100
-print(F" Profit - {profit}")
-Loss = cost_price - selling_price
-Loss = Loss / selling_price * 100
-print(F" Loss - {Loss}")
+principal = 10000
+rate = 5
+time = 2
+SI = principal * rate * time /100
+print(F"Simpe Intrest - {SI}")
