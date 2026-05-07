9e3dbb361364b32478aebcabf5fef01e3e911576
unknown <shubhambindal2999@gmail.com>
2026-05-07 16:24:57 +0530

    area circle programe added

---
+++ 01-Assignment-variables/Python-Assignment-02/area-of-circle.py
@@ -0,0 +1,10 @@
+# 4. Task: Calculate the Area of a Circle 
+# ● Objective: Write a program to calculate the area of a circle. 
+# ● Hints: 
+# ○ Ask the user to input the radius of the circle. 
+# ○ Calculate the area of the circle using the formula: Area = π * radius^2. 
+# ○ Display the calculated area. 
+
+radius =int(input("Enter Radius of the Circle - "))
+area_circle = 3.14 * radius**2
+print(f" Area of Circle - {area_circle}")
