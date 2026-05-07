a0c2af3cc7e2f1e56a65531f1fc0da17c4852e5a
unknown <shubhambindal2999@gmail.com>
2026-05-07 16:31:08 +0530

    added

--- 01-Assignment-variables/Python-Assignment-02/tempCodeRunnerFile.py
+++ 01-Assignment-variables/Python-Assignment-02/tempCodeRunnerFile.py
@@ -1,10 +1,3 @@
-player1 = int(input("Player1 Run Score -"))
-player2 = int(input("Player2 Run Score -"))
-player3 = int(input("Player3 Run Score -"))
-player4 = int(input("Player4 Run Score -"))
-player5 = int(input("Player5 Run Score -"))
-
-total_runs = player1 + player2 + player3 + player4 + player5
-avg_score = player1 + player2 + player3 + player4 + player5 / 5
-print(f"Total Runs - {total_runs}")
-print(f"Average Score - {avg_score}")
+radius =int(input("Enter Radius of the Circle - "))
+area_circle = 3.14 * radius**2
+print(f" Area of Circle - {area_circle}")
